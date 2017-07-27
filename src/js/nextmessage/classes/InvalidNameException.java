7e3646afbcf100b8d045384f0cec5b6ce3f4f7fd
Jaret Stillman <jrsstill@umich.edu>
2017-07-27 15:21:14 -0400

    Adding v1.0 Files
    - No GUI yet besides popup window with instructions
    - Fully interactive texting, but have to manually set ngrok (looking how to do that)
    - Needs testing

---
+++ src/js/nextmessage/classes/InvalidNameException.java
@@ -0,0 +1,21 @@
+package js.nextmessage.classes;
+
+/*
+ * Description: Exception for invalid name format
+ * 
+ * Author: Jaret Stillman (jrsstill@umich.edu)
+ */
+
+public class InvalidNameException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	private String msg;
+	public InvalidNameException()
+	{
+		msg = "Invalid Name!";
+	}
+	public void printStackTrace()
+	{
+		System.out.println(msg);
+	}
+}
